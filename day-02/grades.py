5cfb782a38f43a2f1a54363681825d30f20f9aab
farshadjv <140260376+farshadjv@users.noreply.github.com>
2026-08-18 14:00:48 +0200

    Add the fourth day-02 training

---
+++ day-02/grades.py
@@ -0,0 +1,11 @@
+Score = int(input("Enter your score: "))
+if 90 <= Score <= 100:
+    print("You got an A grade.")
+elif 80 <= Score:
+    print("You got a B grade.")
+elif 70 <= Score:
+    print("You got a C grade.") 
+elif 60 <= Score:
+    print("You got a D grade.")
+else:
+    print("You got an F grade.")
